e738e8bf0c7c5af0378843da292d6676f0ea5304
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-03-08 13:25:09 +0200

    Add files via upload

--- Lesson 5/Task 2/Program.cs
+++ Lesson 5/Task 2/Program.cs
@@ -1,27 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Task_2
+namespace Task2
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Введите число для проверки:");
-            //Создание переменной типа byte и инициализация его значением, которое ввел пользователь, конвертируемым в в byte.
-            byte x = Convert.ToByte(Console.ReadLine());
-            byte n = (byte)(x << 7);//логический сдвиг числа
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               #region Первый способ определения четности и нечетности числа
 
-            if (n == 0)
-            {
-                Console.WriteLine("Число четное");
-            }
-            else
-            {
-                Console.WriteLine("Число не четное");
-            }
+               int a = 0;
+               Console.WriteLine("Первый способ: \nВведите пожалуйста любое число:");
+               int b = Convert.ToInt32(Console.ReadLine());
 
-            // Delay.
-            Console.ReadKey();
-        }
-    }
+               a = b & 0x01;
+
+               if (a == 0)
+               { Console.WriteLine("Число {0} является четным, младший бит равен {1}\n", b, a); }
+
+               else { Console.WriteLine("Число {0} является нечетным, младший бит равен {1}\n", b, a); }
+               #endregion
+
+               
+               
+               #region Второй способ определения четности и нечетности числа
+               int c = 0;
+               Console.WriteLine("Второй способ: \nВведите пожалуйста любое число:");
+               int d = Convert.ToInt32(Console.ReadLine());
+
+               c = d % 2; // Если числитель кратен двойке, то остаток должен быть равен нулю. Иначе единице.
+
+               if (c == 0)
+               { Console.WriteLine("Число {0} является четным, младший бит равен {1}\n", d, c); }
+
+               else { Console.WriteLine("Число {0} является нечетным, младший бит равен {1}\n", d, c); }
+
+               #endregion
+
+
+               
+               #region Третий способ определения четности и нечетности числа
+               
+               Console.WriteLine("Третий способ: \nВведите пожалуйста любое число:");
+               int f = Convert.ToInt32(Console.ReadLine());
+
+               int h = f << 0x07;
+
+
+               /*
+               0011 0101 - непарное число
+               <<        - сдвиг бит влево на 7 позиций
+               1000 0000 - число не равно нулю
+
+               а если парное число
+
+               0011 0100
+               <<
+               0000 0000 - число равно нулю и является кратным двойке, тоесть четным       
+               */
+
+
+                              if ((byte)h == 0)
+                              { Console.WriteLine("Число {0} является четным и равно {1:X}\n", f, (byte)h); }
+
+                              else { Console.WriteLine("Число {0} является нечетным и равно {1:X}\n", f, (byte)h); }
+
+               #endregion
+
+
+               Console.ReadKey();
+
+          }
+     }
 }
