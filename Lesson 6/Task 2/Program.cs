6b4d8111c2961954647d5794007bba41d5315bf1
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-03-10 11:20:47 +0200

    Add files via upload

--- Lesson 6/Task 2/Program.cs
+++ Lesson 6/Task 2/Program.cs
@@ -1,38 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//Дано два числа A и B (A<B) выведите суму всех чисел расположенных между данными числами на экран. 
-//Дано два числа A и B (A<B) выведите все нечетные значения, расположенные между данными числами. 
-
-namespace Task_2
-{
+namespace Task2
+{    /*Используя Visual Studio, создайте проект по шаблону Console Application.
+Дано два числа A и B (A<B) выведите суму всех чисел расположенных между данными числами на экран.
+Дано два числа A и B (A<B) выведите все нечетные значения, расположенные между данными числами.*/
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое число:");
-            double x = Convert.ToDouble(Console.ReadLine());
+            Again: //Метка возврата в первоначальное состояние
 
-            Console.WriteLine("Введите второе число:");
-            double y = Convert.ToDouble(Console.ReadLine());
+            #region Сумма чисел в диапазоне чисел от A до B
 
-            double result = 0;
+            Console.WriteLine("Введите пожалуйста два числа: A и B, чтобы A было меньше B");
+            Console.Write("Число A равно: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Число B равно: ");
+            int B = Convert.ToInt32(Console.ReadLine());
 
-            for (double i = ++x; x < y; x++)
+            int sum = 0;
+
+            if (A < B)
             {
-                //Ниже представлен алгоритм проверки числа на нечетность
-                if ((x % 2) != 0)
+                for (++A; A < B; ++A)
                 {
-                    //Запись нечетного числа в стандартный выходной поток
-                    Console.WriteLine("{0} ", x);
+                    sum += A;
                 }
-                //Суммирование всех чисел расположенных между числами x и y.
-                result += x;
+                Console.WriteLine("\nСумма всех чисел между A и B равна: {0}\n", sum);
+            }
+            else
+            {
+                Console.WriteLine("\n!!!Введите пожалуйста другое число A, чтобы оно было меньше числа B!!!\n\n");
+                goto Again;
             }
 
-            Console.WriteLine("Сумма чисел равна {0}", result);
+            #endregion
 
-            // Delay.
+            #region Все нечетные числа в диапазоне чисел от A до B
+            Console.Write("\nЧисло A равно: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Число B равно: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nВсе нечетные числа между A и В: ");
+
+            if (A < B)
+            {
+                for (++A; A < B; ++A)
+                {
+                    {
+                        if (A % 2 != 0)
+                        {
+                            Console.Write("{0}; ", A);
+                        }
+                    }
+                    continue;
+                }
+            }
+            else
+            {
+                Console.WriteLine("!!!Введите пожалуйста другое число A, чтобы оно было меньше числа B!!!");
+            }
+            Console.WriteLine("\n\n__________________________________________________________________\n\n");
+
+            #endregion
+
             Console.ReadKey();
+            goto Again;
+
+            //Console.ReadKey();
         }
-    }
+    
+}
 }
