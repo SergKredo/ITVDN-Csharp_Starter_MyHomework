8d268c7f6a09146e8d64fe0d7ccf1be970db5c88
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-03-10 13:12:54 +0200

    Add files via upload

---
+++ Lesson 9/Additional1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional1
+{
+            /*Используя Visual Studio, создайте проект по шаблону ConsoleApplication.
+            Создайте массив размерностью в 10 элементов, выведите на экран все элементы массива в обратном
+            порядке.*/                      
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] massive = new byte[10];
+
+            for (byte i = 0; i < 10; )
+            {
+                massive[i] = ++i;
+            }
+            Console.WriteLine(new string('-', 70));
+            Console.Write("Числа массива в обратном порядке: ");
+            for (byte i = 10; i > 0; i--)
+            {
+                Console.Write(massive[i-1]+"; ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
